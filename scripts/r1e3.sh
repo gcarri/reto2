501695e9947d81002678abe7af99f7bf33b39fa5
Gabriel Carrillo <gabrielcarrillo123@gmail.com>
2022-08-11 18:49:44 -0300

    ADD: Solucion reto 1 ejercicio 3

---
+++ scripts/r1e3.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Guardamos el parametro recivido
+PARAMETRO="$*"
+
+# Guadamos la ruta de donde estamos parados
+DIR=$(pwd)
+
+# Creamos el directorio "foo" en el lugar donde estamos parados.
+mkdir $DIR/foo
+
+# y los subdirectorios "dummy" y "empty"
+mkdir $DIR/foo/empty
+mkdir $DIR/foo/dummy
+
+# Creamos los archivos "file1.txt" y "file2.txt"
+touch $DIR/foo/dummy/file1.txt
+touch $DIR/foo/dummy/file2.txt
+
+# Escribimos el "mensaje" en "file1.txt"
+if [[ -z $PARAMETRO ]]; then
+    echo Me encanta bash\!\! > $DIR/foo/dummy/file1.txt
+else
+    echo $PARAMETRO > $DIR/foo/dummy/file1.txt
+fi
+
+# Ejecutamos el script "r1e2.sh"
+bash $DIR/r1e2.sh
